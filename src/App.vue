e474a87b2f4d623f122fcf826d0371dd30a939d0
achuikov <tank4444@mail.ru>
2019-03-27 17:35:29 +0700

    Что бы откатится

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <v-toolbar app dark class="primary">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title v-text="'No Name'"></v-toolbar-title>
+        <v-autocomplete
+                v-model="select"
+                :loading="loading"
+                :items="items"
+                :search-input.sync="search"
+                cache-items
+                class="mx-3"
+                flat
+                hide-no-data
+                hide-details
+                label="What state are you from?"
+                solo-inverted
+        ></v-autocomplete>
+      </v-toolbar>
+      <v-navigation-drawer
+              v-model="drawer"
+              absolute
+              temporary
+      >
+        <v-list>
+          <v-list-tile
+                  v-for="menu in getMenu"
+                  :key="menu.title"
+                  :to="menu.link"
+          >
+            <v-list-tile-action>
+              <v-icon>{{ menu.icon }}</v-icon>
+
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ menu.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-content overflow-hidden>
+        <router-view/>
+      </v-content>
+      <Footer/>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer";
+
+import {TokenService} from './services/storage.service'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Footer
+  },
+  data: () =>({
+    drawer: null,
+    simpleMenu:[
+      {
+        title:'home',
+        icon:'dashboard',
+        link:'/'
+    },{
+      title:'about',
+        icon:'info',
+        link: '/about'
+    },
+    ],
+    registrationMenu:[
+      {
+        title:'signin',
+        icon:'face',
+        link: '/signin'
+      },
+      {
+        title:'signup',
+        icon:'touch_app',
+        link:'/signup'
+      }
+    ],
+  }),
+  method:{
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  },
+  computed:{
+    getMenu:function () {
+      let men=[]
+      men=men.concat(this.simpleMenu)
+      if(!TokenService.getToken()) {men=men.concat(this.registrationMenu)}
+
+
+      return men
+    }
+  }
+
+
 }
+
+
+
+</script>
+<style>
+
 </style>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>log in</h1>
+    </div>
+</template>
+<script>
+    import { mapGetters, mapActions } from "vuex";
+    export default {
+        name: "login",
+        data() {
+            return {
+                email: "",
+                password: "",
+            };
+        },
+        computed: {
+            ...mapGetters('auth', [
+                'authenticating',
+                'authenticationError',
+                'authenticationErrorCode'
+            ])
+        },
+        methods: {
+            ...mapActions('auth', [
+                'login'
+            ]),
+            handleSubmit() {
+                // Perform a simple validation that email and password have been typed in
+                if (this.email != '' && this.password != '') {
+                    this.login({email: this.email, password: this.password})
+                    this.password = ""
+                }
+            }
+        }
+    };
+</script>
